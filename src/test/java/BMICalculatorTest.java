847500bd153c6cbf29b5d539bbea641b76501be1
Simon <s44650@edu.campus02.at>
2025-01-26 19:25:53 +0100

    Verfassen der Tests für Konstruktor

---
+++ src/test/java/BMICalculatorTest.java
@@ -0,0 +1,42 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BMICalculatorTest {
+
+    BMICalculator bmiCalculator = new BMICalculator("Max", "Mustermann", 179, 81.9, 'M');
+
+    @Test
+    @DisplayName("Test Constructor: Firstname")
+    void testFirstname() {
+        assertEquals("Max", bmiCalculator.getFirstname());
+    }
+
+    @Test
+    @DisplayName("Test Constructor: Lastname")
+    void testLastname() {
+        assertEquals("Mustermann", bmiCalculator.getLastname());
+    }
+
+    @Test
+    @DisplayName("Test Constructor: Body Height")
+    void testBodyHeight() {
+        assertEquals(179, bmiCalculator.getBodyHeight());
+    }
+
+    @Test
+    @DisplayName("Test Constructor: Body Weight")
+    void testBodyWeight() {
+        assertEquals(81.9, bmiCalculator.getBodyWeight());
+    }
+
+    @Test
+    @DisplayName("Test Constructor: Gender")
+    void testGender() {
+        assertEquals('M', bmiCalculator.getGender());
+    }
+
+
+}
